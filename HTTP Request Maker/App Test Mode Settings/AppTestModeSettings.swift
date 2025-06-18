ebe473f0d60aa7c6c1dc1b746ef8eb0f83ac5ca8
Anton Zyryanov <antonzyryanov99@gmail.com>
2025-06-18 14:21:15 +0300

    Перевод приложения в тестовый режим

--- HTTP Request Maker/App Test Mode Settings/AppTestModeSettings.swift
+++ HTTP Request Maker/App Test Mode Settings/AppTestModeSettings.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class AppTestModeSettings {
-    var isMockNetworkLayerOn = false
+    var isMockNetworkLayerOn = true
     var mockServerResponseDelayTime: Double = 4.0
     var mockRequestCanFail = true
     var mockSuccessfullServerResponsesConfiguration: MockSuccessfullServerResponsesConfiguration =
